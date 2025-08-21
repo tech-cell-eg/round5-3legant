4ded417c0460fd2bdbaa7531af9eab4d6a4020fa
AbanoubBekhiet <abanwbebekhiet@gmail.com>
2025-08-21 13:46:22 +0300

    some chnages

--- database/seeders/ProductImagesSeeder.php
+++ database/seeders/ProductImagesSeeder.php
@@ -14,27 +14,27 @@ class ProductImagesSeeder extends Seeder
     public function run(): void
     {
         $productImage=new ProductImage();
-        $productImage->product_id=1;
+        $productImage->product_variation_id=1;
         $productImage->image="product_images/p1.jpg";
         $productImage->save();
 
         $productImage=new ProductImage();
-        $productImage->product_id=2;
+        $productImage->product_variation_id=2;
         $productImage->image="product_images/p2.jpg";
         $productImage->save();
 
         $productImage=new ProductImage();
-        $productImage->product_id=3;
+        $productImage->product_variation_id=3;
         $productImage->image="product_images/p3.jpg";
         $productImage->save();
 
         $productImage=new ProductImage();
-        $productImage->product_id=4;
+        $productImage->product_variation_id=4;
         $productImage->image="product_images/p4.jpg";
         $productImage->save();
 
         $productImage=new ProductImage();
-        $productImage->product_id=5;
+        $productImage->product_variation_id=5;
         $productImage->image="product_images/p5.jpg";
         $productImage->save();
 
