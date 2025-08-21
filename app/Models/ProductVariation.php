aabd8439fcb089796031ce4d7c185c786d71a7a9
AbanoubBekhiet <abanwbebekhiet@gmail.com>
2025-08-21 14:11:42 +0300

    coflict

--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -14,4 +14,8 @@ class ProductVariation extends Model {
     public function images() {
         return $this->hasMany(ProductImage::class);
     }
+    //This function will return true if the quantity you want to store is less than the product quantity
+    public function checkTheStock($quantity) {
+        return ($quantity <= $this->quantity);
+    }
 }
